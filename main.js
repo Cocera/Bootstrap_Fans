9ee7d641279608315ba7e126a0def7c53d5361c1
Cocera <alcocera@gmail.com>
2023-10-06 17:25:58 +0200

    JS function cards

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
 const userName = document.getElementById("userName");
 const userMail = document.getElementById("userMail");
-const name = document.getElementById("userName");
-const email = document.getElementById("userMail");
+// const name = document.getElementById("userName");
+// const email = document.getElementById("userMail");
 const pass1 = document.getElementById("inputPass")
 const pass2 = document.getElementById("inputPass2")
 const btn = document.getElementById("btnSend");
-const cardFans = document.getElementById("cardFans")
-
+const divFans = document.getElementById("divFans")
 
 
 
 // ------------------------------------- Álvaro
-const arrayUsers = [];
 
-const showUsers = () => {
+const arrayUsers = JSON.parse(localStorage.getItem("localStorageUsers")) || [];
+
+let showUsers = () => {
     const dataFans = JSON.parse(localStorage.getItem("localStorageUsers"));
-    dataFans.forEach(fan => cardFans.innerHTML += `Datos: ${fan.name}`)
-}
+    //dataFans.forEach(user => divFans.innerHTML += `Datos: ${user.name}`)
+    for (let i = 0; i < dataFans.length; i++) {
+        const lista =
+            `<ul>
+                <li> ${dataFans[i].name}  </li>
+                <li> ${dataFans[i].mail} </li>
+            </ul>`;
+        divFans.innerHTML += lista;
+    }
+    
+};
 
-const saveUsers = (e) => {
+
+const saveUsers = (e) => {   
     e.preventDefault();
 
     const userRandom = {name: userName.value, mail: userMail.value};
     arrayUsers.push(userRandom);
     
     localStorage.setItem("localStorageUsers", JSON.stringify(arrayUsers));
-
-    
+    showUsers()
 };
 
-btn.addEventListener("click", saveUsers);
+btn?.addEventListener("click", saveUsers);
 
+showUsers()
 
 
 // ---------------------------------------- Francesc
