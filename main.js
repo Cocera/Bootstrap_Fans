0658772c3fb4ab648232d561fe7a77e56ee878ae
Cocera <alcocera@gmail.com>
2023-10-07 18:50:34 +0200

    function avatarProfile

--- main.js
+++ main.js
@@ -11,12 +11,19 @@ const divFans = document.getElementById("divFans")
 
 const arrayUsers = JSON.parse(localStorage.getItem("localStorageUsers")) || [];
 
+const avatarProfile = () => {
+    const arrAvatarProfile = ['avatar_1.png', 'avatar_2.png']
+    let num = Math.floor( Math.random()*arrAvatarProfile.length)
+    let img = `./assets/avatar_pics/${arrAvatarProfile[num]}`
+    console.log(img)
+    return img
+};
 const showUsers = () => {
     arrayUsers.forEach(user => {
         divFans.innerHTML += `
         <div class="col">
             <div class="card rounded-4 shadow-sm">
-                <img src="{none}" class="card-img-top" alt="...">
+                <img src="${avatarProfile()}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${user.name}</h5>
                     <p class="card-text">Mail: ${user.mail}</p>
