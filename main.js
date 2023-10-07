7c6d3e1998b4ddd0ca531654cdc10ee69718d81f
Cocera <alcocera@gmail.com>
2023-10-07 13:05:45 +0200

    added fan cards to showUsers function and column class in DOM element

--- main.js
+++ main.js
@@ -14,11 +14,13 @@ const arrayUsers = JSON.parse(localStorage.getItem("localStorageUsers")) || [];
 let showUsers = () => {
     arrayUsers.forEach(user => {
         divFans.innerHTML += `
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-               <h5 class="card-title">${user.name}</h5>
-               <p class="card-text">Mail: ${user.mail}</p>
+        <div class="col">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${user.name}</h5>
+                <p class="card-text">Mail: ${user.mail}</p>
+                </div>
             </div>
         </div>`;
     });
