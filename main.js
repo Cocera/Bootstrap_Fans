e913a6d2e141e96596a61ed686692cf875447774
Francesc Alberola <f.alberola@gmail.com>
2023-10-08 04:15:37 +0200

    form-css-conflicto_print-avatar-error-rellenar

--- main.js
+++ main.js
@@ -1,10 +1,109 @@
 const userName = document.getElementById("userName");
 const userMail = document.getElementById("userMail");
-const pass1 = document.getElementById("inputPass")
-const pass2 = document.getElementById("inputPass2")
 const btn = document.getElementById("btnSend");
 const divFans = document.getElementById("divFans")
 
+// ---------------------------------------- Francesc
+
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+	userName: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	userMail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+}
+
+const campos = {
+	userName: false,
+	userMail: false,
+	password: false,
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "userName":
+			validarCampo(expresiones.userName, e.target, "userName");
+		break;
+        case "userMail":
+            validarCampo(expresiones.userMail, e.target, "userMail");
+        break;
+		case "password":
+			validarCampo(expresiones.password, e.target, "password");
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-correcto");
+		document.querySelector(`#grupo__${campo} i`).classList.add("fa-check-circle");
+		document.querySelector(`#grupo__${campo} i`).classList.remove("fa-times-circle");
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-incorrecto");
+		document.getElementById(`grupo__${campo}`).classList.remove("formulario__grupo-correcto");
+		document.querySelector(`#grupo__${campo} i`).classList.add("fa-times-circle");
+		document.querySelector(`#grupo__${campo} i`).classList.remove("fa-check-circle");
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
+		campos[campo] = false;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById("password");
+	const inputPassword2 = document.getElementById("password2");
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos["password"] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos["password"] = true;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener("keyup", validarFormulario);
+	input.addEventListener("blur", validarFormulario);
+});
+
+formulario.addEventListener("submit", (e) => {
+	e.preventDefault();
+});
+	const terminos = document.getElementById("terminos");
+	if(campos.userName && campos.userMail && campos.password && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 3000);
+		window.location.href = "/fans.html"; 3000;
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+
+
+// --------------------------------------
 
 
 // ------------------------------------- Álvaro
@@ -34,7 +133,6 @@ const showUsers = () => {
     });
 };
 
-
 const saveUsers = (e) => {   
     e.preventDefault();
     const userRandom = {name: userName.value, mail: userMail.value};
@@ -48,9 +146,4 @@ btn?.addEventListener("click", saveUsers);
 showUsers()
 
 
-// ---------------------------------------- Francesc
-
-
-
-// -----------------------------------------
 
