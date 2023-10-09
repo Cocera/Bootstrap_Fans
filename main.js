4d4909318fe1e491fab4104b6ee80eb766118322
Luciana Boasso <luboasso16@gmail.com>
2023-10-09 16:07:09 +0200

    commit de prueba

--- main.js
+++ main.js
@@ -82,29 +82,29 @@ const loadUsers = () => {
 
 loadUsers();
 
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+// //Get the button
+// let mybutton = document.getElementById("btn-back-to-top");
 
 
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 20 ||
+//     document.documentElement.scrollTop > 20
+//   ) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
 
-mybutton.addEventListener("click", backToTop);
+// mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function backToTop() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
