892a5bfab700a5d83bd57a2604d231dc94146a98
Luciana Boasso <luboasso16@gmail.com>
2023-10-09 15:50:39 +0200

    Cambiar bg color

--- main.js
+++ main.js
@@ -84,3 +84,30 @@ function loadUsers() {
 
 // Cargar usuarios al cargar la página
 loadUsers();
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
